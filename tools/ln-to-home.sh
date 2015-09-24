0a960c6f351ee9be36688dadcf32aa763143ce68
19000 <19000.0rpm@gmail.com>
2015-09-25 06:31:39 +0800

    ln-tool.sh: Code improve

--- tools/ln-to-home.sh
+++ tools/ln-to-home.sh
@@ -2,10 +2,16 @@
 
 DIR=`pwd`
 
-echo '.vimrc
+echo '\
+.vimrc
 .vim
 .emacs.d
 .gitconfig
-.tmux.conf' | while read F; do 
-  [ -f $HOME/$F -o -d $HOME/$F ] && echo "Fail: already exists $HOME/$F" || (ln -s $DIR/$F $HOME/$F && echo "Success: created link $HOME/$F -> $DIR/$F")
+.tmux.conf' |
+while read F; do 
+  if [ -f $HOME/$F -o -d $HOME/$F ]; then 
+    echo "Fail: already exists $HOME/$F";
+  else 
+    ln -s $DIR/$F $HOME/$F && echo "Success: created link $HOME/$F -> $DIR/$F";
+  fi
 done
