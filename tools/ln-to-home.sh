ec2886bbea2776dbb54390c733c5feba45bd027e
19000 <19000.0rpm@gmail.com>
2015-09-25 05:55:41 +0800

    ln-tool: add gitconfig, tmux.conf

--- tools/ln-to-home.sh
+++ tools/ln-to-home.sh
@@ -4,6 +4,8 @@ DIR=`pwd`
 
 echo '.vimrc
 .vim
-.emacs.d' | while read F; do 
+.emacs.d
+.gitconfig
+.tmux.conf' | while read F; do 
   [ -f $HOME/$F -o -d $HOME/$F ] && echo "Fail: already exists $HOME/$F" || (ln -s $DIR/$F $HOME/$F && echo "Success: created link $HOME/$F -> $DIR/$F")
 done
