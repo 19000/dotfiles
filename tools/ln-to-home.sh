49a17804289e552ccb664f8781981b3d01fa4feb
19000 <19000.0rpm@gmail.com>
2015-09-25 05:37:37 +0800

    Auto ln tool: add message output

--- tools/ln-to-home.sh
+++ tools/ln-to-home.sh
@@ -5,5 +5,5 @@ DIR=`pwd`
 echo '.vimrc
 .vim
 .emacs.d' | while read F; do 
-  [ -f $HOME/$F -o -d $HOME/$F ] || ln -s $DIR/$F $HOME/$F;
+  [ -f $HOME/$F -o -d $HOME/$F ] && echo "Fail: already exists $HOME/$F" || (ln -s $DIR/$F $HOME/$F && echo "Success: created link $HOME/$F -> $DIR/$F")
 done
